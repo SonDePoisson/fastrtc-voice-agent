2fe00727f77cd5c84742dc6c0fc167164cf4994b
SonDePoisson <clement.poissoncornu@gmail.com>
2025-12-28 22:37:04 +0100

    Get Ollama answer

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 from fastrtc import get_tts_model, ReplyOnPause, Stream
 import numpy as np
 from whisper import load_model, load_audio, Whisper
+import ollama
 
 STT_MODEL = "small"
+OLLAMA_MODEL = "ministral-3"
 
 
 class STTModel:
@@ -10,6 +12,8 @@ class STTModel:
         self.model: Whisper = load_model(name=STT_MODEL, device="cpu")
 
     def stt(self, audio: np.ndarray) -> str:
+        # Convert int16 to float32 and normalize to [-1, 1]
+        audio = audio.astype(np.float32).flatten() / 32768.0
         result = self.model.transcribe(audio, fp16=False)
         return result["text"]
 
@@ -17,19 +21,20 @@ class STTModel:
         return load_audio(path)
 
 
-def main():
-    # Create the model (downloads from HF if not cached)
-    stt_model = STTModel()
+def response(audio: tuple[int, np.ndarray]):
+    sample_rate, audio_data = audio
+    user_prompt = stt_model.stt(audio_data)
+    print(f"User: {user_prompt}")
+    ollama_response = ollama.generate(model=OLLAMA_MODEL, prompt=user_prompt)
+    print(f"Assistant: {ollama_response.response}")
+    # for audio_chunk in tts_model.stream_tts_sync(prompt):
+    #     yield audio_chunk
 
-    # print("Recording ...")
-    # audio_data = np.zeros(3 * 16000, dtype=np.float16)  # 3 second of silence
 
-    audio_data = stt_model.load_audio("Spiderman_AudioTestFile.mp3")
+stt_model = STTModel()
 
-    # Transcribe
-    text = stt_model.stt(audio_data)
-    print(f"Transcription: {text}")
+stream = Stream(ReplyOnPause(response), modality="audio", mode="send")
 
 
 if __name__ == "__main__":
-    main()
+    stream.ui.launch()
